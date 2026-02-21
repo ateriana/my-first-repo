56f7b8640531b1fb910847737f84027b0f097d92
Ateriana <146635693+ateriana@users.noreply.github.com>
2026-02-21 22:44:10 +0100

    Update 2-print_comb2.py

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(99):
     print(f"{str(num).zfill(2)}", end=', ')
-print(f"{str(99)}\n")
+print(f"{str(99)}")
 
 #This task seems to be broken
 #The correction complains about the number of print functions, number of loops, about using a variable and the output
