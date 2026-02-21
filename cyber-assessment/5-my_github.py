b2b5ac30a2fa6d40372af107253d959d4e399745
Ateriana <146635693+ateriana@users.noreply.github.com>
2026-02-21 22:32:49 +0100

    Update 5-my_github.py

--- cyber-assessment/5-my_github.py
+++ cyber-assessment/5-my_github.py
@@ -5,11 +5,21 @@ import requests
 username = sys.argv[1]
 password = sys.argv[2]
 
+#First
+headers = {
+    'Authorization': f'Bearer {password}',
+    'X-GitHub-Api-Version': '2022-11-28',
+}
+
+response = requests.get('https://api.github.com/octocat', headers=headers)
+
+print(response)
+
 headers = {
     'Accept': 'application/vnd.github+json',
     'X-GitHub-Api-Version': '2022-11-28',
 }
 
-response = requests.get('https://api.github.com/user', headers=headers)
+user_info = requests.get('https://api.github.com/user', headers=headers)
 
-print(response)
+print(user_info)
